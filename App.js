077119c4351dc6a4a3aa484da35a7a3b58801447
003 <apple@003-MacBook-Air.local>
2024-09-12 12:37:37 -0700

    add TextInput and useState hoot

--- App.js
+++ App.js
@@ -1,13 +1,29 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import Header from "./Components/Header";
+import { useState } from "react";
+import { TextInput } from 'react-native';
+
 
 export default function App() {
   const appName = "My awesome app";
+  const [text, setText] = useState("Hello");
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-      <Header name={appName}/>
+      <Header name={appName}>
+        <Text>child 1</Text>
+        <Text>child 2</Text>
+      </Header>
+      <TextInput
+        placeholder="Type something"
+        keyboardType="default"
+        style={{ borderBottonColor: "purple", borderBottomWidth: 2 }}
+        value={text}
+        onChangeText={function (changedText) {
+          setText(changedText);
+        }}
+      />
     </View>
   );
 }
