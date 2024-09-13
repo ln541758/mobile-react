5ffdd8fb2fc410e3ad3f41d5efb729bff8abb58d
003 <apple@003-MacBook-Air.local>
2024-09-12 21:13:13 -0700

    Add a prop to control focus

--- Components/Input.js
+++ Components/Input.js
@@ -1,14 +1,22 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TextInput } from "react-native";
 import React from "react";
-import { useState } from "react";
-import { TextInput } from "react-native";
+import { useState, useEffect, useRef } from "react";
 
-export default function Input() {
-  const [text, setText] = useState("Hello");
 
+export default function Input({ autoFocus }) {
+  const [text, setText] = useState("");
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    if (autoFocus && inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, [autoFocus]);
+  
   return (
     <View>
       <TextInput
+        ref={inputRef}
         placeholder="Type something"
         keyboardType="default"
         style={{ borderBottonColor: "purple", borderBottomWidth: 2 }}
