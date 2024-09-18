6b5e66c6fe92785acc9533f05a7954e425c89ff0
003 <apple@003-MacBook-Air.local>
2024-09-17 22:14:14 -0700

    Clear the TextInput after pressing buttons

--- Components/Input.js
+++ Components/Input.js
@@ -52,7 +52,7 @@ export default function Input({
         text: "OK",
         onPress: () => {
           cancelHandler();
-          setText("");
+          setText('');
         },
       },
     ]);
