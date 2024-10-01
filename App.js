e1dedb58947bac57ef3f55d4968edfa5ebd2320b
003 <apple@003-MacBook-Air.local>
2024-10-01 13:45:35 -0700

    GoalDetails Component

--- App.js
+++ App.js
@@ -1,14 +1,24 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import Home from "./Components/Home";
+import GoalDetails from "./Components/GoalDetails";
 import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+console.log(Stack);
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Home />
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Details" component={GoalDetails} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
+
+
 
 const styles = StyleSheet.create({});
