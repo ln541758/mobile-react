9c11c1fc08cbaf0deca51032b03fa5b61c6c9b01
003 <apple@003-MacBook-Air.local>
2024-10-15 13:27:50 -0700

    Managing API keys for Firebase

--- Firebase/firebaseSetup.js
+++ Firebase/firebaseSetup.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBSt6HZMzYzGejS-RnFl1o_-nyCEHc5ks0",
-  authDomain: "mobile-768e7.firebaseapp.com",
-  projectId: "mobile-768e7",
-  storageBucket: "mobile-768e7.appspot.com",
-  messagingSenderId: "897616221263",
-  appId: "1:897616221263:web:10194442881d6e005e863a"
+  apiKey: process.env.EXPO_PUBLIC_apiKey,
+  authDomain: process.env.EXPO_PUBLIC_authDomain,
+  projectId: process.env.EXPO_PUBLIC_projectId,
+  storageBucket:  process.env.EXPO_PUBLIC_storageBucket,
+  messagingSenderId: process.env.EXPO_PUBLIC_messagingSenderId,
+  appId: process.env.EXPO_PUBLIC_appId
 };
 
 // Initialize Firebase
