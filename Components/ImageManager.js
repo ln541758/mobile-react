2c945fe00355778663a3c5070e0face9888f388a
003 <apple@003-MacBook-Air.local>
2024-11-06 03:53:30 -0800

    Ask for permission

--- Components/ImageManager.js
+++ Components/ImageManager.js
@@ -1,13 +1,17 @@
 import { Button, StyleSheet, Text, View } from "react-native";
 import React from "react";
 import * as ImagePicker from "expo-image-picker";
+import { useCameraPermissions } from "expo-image-picker";
 
 export default function ImageManager() {
+  const [response, requestPermission] = ImagePicker.useCameraPermissions();
+
   async function takeImageHandler() {
     try {
       const result = await ImagePicker.launchCameraAsync({
         allowsEditing: true,
       });
+      console.log(result.assets[0].uri);
     } catch (err) {
       console.log(err);
     }
